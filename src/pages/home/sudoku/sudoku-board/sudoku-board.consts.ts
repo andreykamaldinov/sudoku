50271d9a2b071f209691fcd1a33ba6636aaade78
andreykamaldinov <andreykama46@gmail.com>
2025-01-21 16:51:00 +0200

    feat(ref: no-ref): added leader board logic

--- src/pages/home/sudoku/sudoku-board/sudoku-board.consts.ts
+++ src/pages/home/sudoku/sudoku-board/sudoku-board.consts.ts
@@ -1,8 +1,8 @@
 import { SudokuLevel } from '@/pages/home/sudoku/sudoku-header/level/level.enum.ts'
 
 export const sudokuVisibleRanges: Record<SudokuLevel, [number, number]> = {
-  [SudokuLevel.BEGINNER]: [80, 80],
-  [SudokuLevel.INTERMEDIATE]: [80, 80],
-  [SudokuLevel.HARD]: [80, 80],
-  [SudokuLevel.EXPERT]: [80, 80],
+  [SudokuLevel.BEGINNER]: [36, 40],
+  [SudokuLevel.INTERMEDIATE]: [32, 36],
+  [SudokuLevel.HARD]: [28, 32],
+  [SudokuLevel.EXPERT]: [24, 28],
 }
