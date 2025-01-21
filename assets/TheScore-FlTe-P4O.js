83bafd4224834dc62d5e29d04fae6d195996387f
andreykamaldinov <andreykamaldinov@users.noreply.github.com>
2025-01-21 17:08:47 +0000

    deploy: b2466359fc022062f9fa99c0d8b2deb4c304fb84

--- assets/TheScore-FlTe-P4O.js
+++
@@ -1,1 +0,0 @@
-import{d as s,u as t,s as r,o as a,c as n,t as c,a as p}from"./index-D65hahSH.js";const i=s({__name:"TheScore",setup(u){const e=t(),{score:o}=r(e);return(_,l)=>(a(),n("div",null,"Score: "+c(p(o)),1))}});export{i as default};
