2bb16d6bde0a954332d8d2d0c9bc585c8724dde0
andreykamaldinov <andreykama46@gmail.com>
2025-01-21 18:57:05 +0200

    feat(ref: no-ref): added undo, redo buttons

--- src/pages/home/sudoku/TheSudoku.vue
+++ src/pages/home/sudoku/TheSudoku.vue
@@ -4,6 +4,8 @@ import { defineAsyncComponent } from 'vue'
 import LeadersBoard from '@/pages/home/sudoku/leaders-board/LeadersBoard.vue'
 import SudokuHeader from '@/pages/home/sudoku/sudoku-header/SudokuHeader.vue'
 
+const UndoRedo = defineAsyncComponent(() => import('@/pages/home/sudoku/undo-redo/UndoRedo.vue'))
+
 const AvailableDigits = defineAsyncComponent(
   () => import('@/pages/home/sudoku/available-digits/AvailableDigits.vue'),
 )
@@ -22,6 +24,7 @@ const SudokuBlock = defineAsyncComponent(
       <HintMessage />
       <div class="flex gap-10 w-full mob:flex-col">
         <div class="flex flex-col gap-5">
+          <UndoRedo />
           <SudokuBlock />
           <AvailableDigits />
         </div>
