fa9cc53995e642a9d7d55a181c13cb01149d7aa0
andreykamaldinov <andreykama46@gmail.com>
2025-01-22 16:32:24 +0200

    feat(ref: no-ref): ui enchantments

--- src/shared/select/BaseSelect.vue
+++ src/shared/select/BaseSelect.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { ref, watch } from 'vue';
+import IconChevron from '@/shared/icons/IconChevron.vue';
 
 type SelectProps = {
     items: string[];
@@ -45,16 +46,7 @@ watch(
             @click="toggleDropdown"
         >
             <span>{{ selected }} </span>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-4 h-4"
-            >
-                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 9l-7.5 7.5L4.5 9" />
-            </svg>
+            <IconChevron class="w-4 h-4" />
         </div>
 
         <div
